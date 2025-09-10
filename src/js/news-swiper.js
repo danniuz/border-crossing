80d05876f9d6425421225cfeb1b785ad4c01c716
dan <danniuz@dans-MacBook-Pro.local>
2025-09-10 16:22:35 +0300

    feature/MODRAMIM-123 Add manager page

--- src/js/news-swiper.js
+++ src/js/news-swiper.js
@@ -1,22 +1,22 @@
 export function initNewsSwiper() {
-  const newsSwiper = document.querySelector('.news-swiper');
+  const newsSwiper = document.querySelector('#news-swiper');
 
   if (!newsSwiper) {
     return;
   }
 
-  const swiper = new Swiper('.news-swiper', {
-      direction: 'horizontal',
-      loop: true,
-    
-      pagination: {
-        el: '.news-swiper__swiper-pagination',
-        clickable: true,
-      },
-    
-      navigation: {
-        nextEl: '.news-swiper__swiper-button-next',
-        prevEl: '.news-swiper__swiper-button-prev',
-      },
-    });
+  const swiper = new Swiper('#news-swiper', {
+    direction: 'horizontal',
+    loop: true,
+
+    pagination: {
+      el: '#news-swiper-swiper-pagination',
+      clickable: true,
+    },
+
+    navigation: {
+      nextEl: '#news-swiper-button-next',
+      prevEl: '#news-swiper-button-prev',
+    },
+  });
 }
