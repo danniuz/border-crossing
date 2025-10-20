4a270c55fd15250ee5c71801cfb59b555c212a76
Danil <danniuz@Danils-MacBook-Pro.local>
2025-10-20 10:05:02 +0300

    wip

---
+++ src/js/place-popup.js
@@ -0,0 +1,68 @@
+import { initPlaceChart } from './init-place-chart.js';
+
+export function initPlacePopup() {
+  const PlacePopupTogglers = document.querySelectorAll('.place-popup-toggler');
+
+  if (!PlacePopupTogglers?.length) {
+    return;
+  }
+
+  fetch('place-popup.html')
+    .then((response) => response.text())
+    .then((html) => {
+      showPopup(html);
+    });
+
+  PlacePopupTogglers.forEach((toggler) => {
+    toggler.addEventListener('click', () => {
+      fetch('place-popup.html')
+        .then((response) => response.text())
+        .then((html) => {
+          showPopup(html);
+        });
+    });
+  });
+}
+
+function showPopup(html) {
+  // Remove any existing popup
+  const existingPopup = document.querySelector('.place-popup__wrapper');
+  if (existingPopup) {
+    closePopup(existingPopup);
+    return;
+  }
+
+  const placePopupWrapper = document.createElement('div');
+  placePopupWrapper.classList.add('place-popup__wrapper');
+  placePopupWrapper.innerHTML = html;
+
+  document.body.appendChild(placePopupWrapper);
+
+  initPlaceChart();
+
+  // Trigger the slide-in animation after a small delay to ensure the element is rendered
+  requestAnimationFrame(() => {
+    placePopupWrapper.classList.add('place-popup__wrapper--open');
+  });
+
+  const closeButton = placePopupWrapper.querySelector('.place-popup__close');
+
+  if (closeButton) {
+    closeButton.addEventListener('click', () => {
+      closePopup(placePopupWrapper);
+    });
+  }
+}
+
+function closePopup(popupElement) {
+  // Add closing class to trigger slide-out animation
+  popupElement.classList.remove('place-popup__wrapper--open');
+  popupElement.classList.add('place-popup__wrapper--closing');
+
+  // Remove the element after animation completes
+  setTimeout(() => {
+    if (popupElement.parentNode) {
+      popupElement.remove();
+    }
+  }, 300); // Match the CSS transition duration
+}
--- src/main.js
+++ src/main.js
@@ -5,30 +5,31 @@ import { initNewsSwiper } from './js/news-swiper.js';
 import { initMarquee } from './js/marquee.js';
 import { initScrollToTop } from './js/scroll-to-top.js';
 import './js/form-interactions.js';
+import { initPlacePopup } from './js/place-popup.js';
 
 import 'mapbox-gl/dist/mapbox-gl.css';
-import './style.scss'
+import './style.scss';
 
 function loadMap() {
-    const map = initMap();
+  const map = initMap();
 
-    if (!map) {
-        return;
-    }
+  if (!map) {
+    return;
+  }
 
-    // map.on('load', () => {
-    //     const wrapper = document.querySelector('#wrapper');
+  // map.on('load', () => {
+  //     const wrapper = document.querySelector('#wrapper');
 
-    //     if (wrapper) {
-    //         wrapper.classList.add('wrapper--visible');
-    //     }
-    // }); 
-
+  //     if (wrapper) {
+  //         wrapper.classList.add('wrapper--visible');
+  //     }
+  // });
 }
 
-initAside({ opened: false });
+initAside({ opened: true });
 loadMap();
 initMarquee({ start: true });
 initFlashes('flashes-faq');
 initNewsSwiper();
 initScrollToTop('#scroll-to-top', 500);
+initPlacePopup();
