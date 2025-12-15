0b1ec8d51901eb53f51bdba08f03817e7812d784
Danil <danniuz@Danils-MacBook-Pro.local>
2025-12-15 14:01:05 +0300

    Improve contact us and news

--- src/js/scroll-to-top.js
+++ src/js/scroll-to-top.js
@@ -3,25 +3,24 @@
  * @param {boolean} params.opened
  */
 export function initScrollToTop(selector, offset) {
-    const scrollToTop = document.querySelector(selector);
+  const scrollToTop = document.querySelector(selector);
 
-    if (!scrollToTop) {
-        return;
+  if (!scrollToTop) {
+    return;
+  }
+
+  window.addEventListener('scroll', () => {
+    if (window.scrollY > offset) {
+      scrollToTop.classList.add('scroll-to-top--visible');
+    } else {
+      scrollToTop.classList.remove('scroll-to-top--visible');
     }
+  });
 
-    window.addEventListener('scroll', () => {
-        if (window.scrollY > offset) {
-            scrollToTop.classList.add('scroll-to-top--visible');
-        } else {
-            scrollToTop.classList.remove('scroll-to-top--visible');
-        }
+  scrollToTop.addEventListener('click', () => {
+    window.scrollTo({
+      top: 0,
+      behavior: 'smooth',
     });
-
-    scrollToTop.addEventListener('click', () => {
-        window.scrollTo({
-            top: 0,
-            behavior: 'smooth'
-        });
-    });
+  });
 }
-
