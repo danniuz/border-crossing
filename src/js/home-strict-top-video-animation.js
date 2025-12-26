58dcc0593641cb3add8b5fc98f28a2b710b617e9
Dan <danniuz@pm.me>
2025-12-26 15:06:36 +0300

    Bump hp with animations

---
+++ src/js/home-strict-top-video-animation.js
@@ -0,0 +1,58 @@
+export function initHomeStrictTopVideoAnimation() {
+    const { gsap } = window;
+    
+    const homeStrictTopVideo = document.getElementById('home-strict-top-video');
+    const homeStrictTopVideoWrapper = document.getElementById('home-strict-top-video-wrapper');
+    const homeStrictTopTextWrapper = document.getElementById('home-strict-top-text-wrapper');
+    const homeStrictTopMenuActions = document.getElementById('home-strict-top-menu-actions');
+
+    if (!homeStrictTopVideoWrapper) {
+        return;
+    }
+
+    let isAnimating = false;
+
+    homeStrictTopVideo.addEventListener('click', () => {
+        if (isAnimating) {
+            return;
+        }
+
+        isAnimating = true;
+
+
+        gsap.to(homeStrictTopVideo, {
+            width: '100vw',
+            duration: 1,
+            ease: 'power2.inOut',
+            onComplete: () => {
+                homeStrictTopVideoWrapper.classList.add('home__strict-top-video-wrapper--animated');
+
+                homeStrictTopVideo.classList.add('home__strict-top-video--animated');
+                    gsap.to(homeStrictTopVideo, {
+                        height: '100%',
+                        duration: 0.8,
+                        ease: 'power2.inOut',
+                        onComplete: () => {
+                            isAnimating = false;
+
+                            gsap.to(homeStrictTopMenuActions, {
+                                opacity: 1,
+                                transform: 'translateY(0)',
+                                duration: 1,
+                                ease: 'power2.inOut',
+                            });
+
+                            gsap.to(homeStrictTopTextWrapper, {
+                                opacity: 1,
+                                duration: 1,
+                                translateY: '-50px',
+                                ease: 'power2.inOut',
+                            });
+
+                        }
+                    });
+            }
+        });
+    });
+}
+
