9fd9fe211932dac745b33c850e6adf460760e8d3
Gennady Vaschenko <vashchenko.gv@ssnab.ru>
2026-01-28 13:13:17 +0300

    feat: fix font-weight on active nav-link

--- src/js/nav-language-dropdown.js
+++ src/js/nav-language-dropdown.js
@@ -1,21 +1,28 @@
 export function initNavLanguageDropdown() {
   const navLanguageMenuLink = document.getElementById('nav-language-menu-link');
-
   const navLanguageDropdown = document.getElementById('nav-language-dropdown');
 
   if (!navLanguageMenuLink || !navLanguageDropdown) {
     return;
   }
 
-
-  navLanguageMenuLink.addEventListener('click', (e) => {
+  navLanguageMenuLink.addEventListener('click', function (e) {
     e.stopPropagation();
-    navLanguageDropdown.classList.toggle('nav-language-dropdown--opened');
+
+    const isOpened = navLanguageDropdown.classList.toggle(
+        'nav-language-dropdown--opened'
+    );
+
+    navLanguageMenuLink.classList.toggle('active', isOpened);
   });
 
-  document.addEventListener('click', (e) => {
-    if (!navLanguageMenuLink.contains(e.target) && !navLanguageDropdown.contains(e.target)) {
+  document.addEventListener('click', function (e) {
+    if (
+        !navLanguageMenuLink.contains(e.target) &&
+        !navLanguageDropdown.contains(e.target)
+    ) {
       navLanguageDropdown.classList.remove('nav-language-dropdown--opened');
+      navLanguageMenuLink.classList.remove('active');
     }
   });
 }
